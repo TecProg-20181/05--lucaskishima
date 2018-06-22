a4de2befddb8212a4c22c09456a064ef8689a478
Lucas Kishima <lucaskishima2@gmail.com>
2018-06-22 19:12:27 -0300

    Testing subprocess_check_output.

--- diskspace/tests.py
+++ diskspace/tests.py
@@ -8,10 +8,17 @@ class TestDiskSpace(unittest.TestCase):
         self.blocks = 8
         self.bytesOfBlocks = '4.00Kb'
 
+        #used in test_bytes_to_readable
+        self.command = "du"
+        self.result = subprocess.check_output(self.command.strip().split(' '))
+
     def test_bytes_to_readable(self):
         command = bytes_to_readable(self.blocks)
         self.assertEqual(command, self.bytesOfBlocks)
 
+    def test_subprocess_check_output(self):
+        command = subprocess_check_output(self.command)
+        self.assertEqual(command, self.result)
 
 if __name__ == '__main__':
     unittest.main()
